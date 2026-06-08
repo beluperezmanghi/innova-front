54bf010cf4472b8ff3258dc5ca38bfcbb9a3904a
beluperezmanghi <beluperezmanghi@gmail.com>
2026-06-08 19:31:47 -0300

    Cambios

--- src/app/modules/join-our-team/join-our-team.ts
+++ src/app/modules/join-our-team/join-our-team.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Footer } from '../shell/footer/footer';
 
 @Component({
   selector: 'app-join-our-team',
   standalone: true,
+  imports: [Footer],   // ← ACÁ
   templateUrl: './join-our-team.html',
   styleUrl: './join-our-team.scss'
 })
